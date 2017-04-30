a8958c7fa0543ed7f3f60833a5c940076b810a51
Jamie Collinson <jamiecollinson@gmail.com>
2017-04-30 12:43:10 +0100

    Add notification on account creation

--- imports/startup/accounts-callbacks.js
+++ imports/startup/accounts-callbacks.js
@@ -1,6 +1,16 @@
 import { Accounts } from 'meteor/accounts-base';
+import { Email } from 'meteor/email';
 
 Accounts.onCreateUser((options, user) => {
+
+    // Notify Catherine
+    Email.send({
+	to: 'catherine.bacon+books@gmail.com',
+	from: 'no-reply@books.catherinebacon.co.uk',
+	subject: `New user created - ${user.username}`,
+	text: `User ${user.username} was created with email address ${user.emails[0].address}`
+    });
+    
   user.fullName = options.fullname || '';
   user.city = options.address || '';
   return user;
